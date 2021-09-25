67e68d51b93c455e71dc93d5b63769a49d91bd88
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-25 20:21:51 +0200

    Project initialized in IntellJ Packages and first problem classes created and commented

---
+++ src/com/jonasermert/einfach/FlickSwitch.java
@@ -0,0 +1,22 @@
+package com.jonasermert.einfach;
+
+/*
+Create a function that always returns true for every item in a given array. However, if an element is the word "flick", switch to always returning the opposite boolean value.
+Examples
+
+switches(["edabit", "flick", "eda", "bit"]) ➞ [true, false, false, false]
+
+switches(["flick", 11037, 3.14, 53]) ➞ [false, false, false, false]
+
+switches([false, false, "flick", "sheep", "flick"]) ➞ [true, true, false, false, true]
+
+Notes
+
+    "flick" will always be given in lowercase.
+    An array may contain multiple flicks.
+    Switch the boolean value on the same element as the flick itself.
+ */
+
+
+public class FlickSwitch {
+}
---
+++ src/com/jonasermert/sehreinfach/HowEdabitWorks.java
@@ -0,0 +1,23 @@
+package com.jonasermert.sehreinfach;
+
+/*
+
+This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+public static boolean returnTrue() {
+
+}
+
+All you have to do is type return true; between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+Notes
+
+    If you get stuck on a challenge, find help in the Resources tab.
+    If you're really stuck, unlock solutions in the Solutions tab.
+
+
+ */
+
+
+
+public class HowEdabitWorks {
+}
